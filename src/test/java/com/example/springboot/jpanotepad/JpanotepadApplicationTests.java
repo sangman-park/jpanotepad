7aed39c1dcccb97bfa4c3d8529a2afa15c9d8f3e
sangman-park <sang.park@vgw.co>
2018-12-21 13:33:47 +0900

    remove global test..

--- src/test/java/com/example/springboot/jpanotepad/JpanotepadApplicationTests.java
+++ src/test/java/com/example/springboot/jpanotepad/JpanotepadApplicationTests.java
@@ -1,10 +1,7 @@
 package com.example.springboot.jpanotepad;
 
-import com.example.springboot.jpanotepad.service.JPATestService;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,18 +11,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("test")
 public class JpanotepadApplicationTests {
 
-	@Autowired
-	JPATestService jpaTestService;
-
 	// Test for Chkeck Module in WebAppRunning
 	@Test
 	public void contextLoads() {
 
-		jpaTestService.jpaUpdateTest();
-		//Check
-		Assert.assertTrue(jpaTestService.findAll("Member").size() > 0);
-		Assert.assertTrue(jpaTestService.findAll("MemberGroup").size() > 0);
-        Assert.assertTrue(jpaTestService.findByNameFromMember("user1").size() >0);
 
 	}
 
